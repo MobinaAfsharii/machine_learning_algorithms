9c4a33a1732a3c87e5ee2b8c7a76ca4d7f87e2a8
Mohammadreza Amani <More.amani@yahoo.com>
2024-01-04 22:45:17 +0330

    initial commit

---
+++ algorithms/linear_regression/linear_regression.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class LinearRegression:
+    def __init__(self) -> None:
+        """
+        this is tozih
+        """
+        ...
+    
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        this is tozih
+        """
+        ...
+    
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        this is tozih
+        """
+        ...
+
+if __name__ == '__main__':
+    ...
